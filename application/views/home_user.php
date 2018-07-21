4ac38a14a9d1b11a3144277d1d5db8df5a8f450e
alfianbastami <ahmadalfianbastami@gmail.com>
2018-07-21 15:23:46 +0700

    view

--- application/views/home_user.php
+++ application/views/home_user.php
@@ -7,26 +7,26 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Footwear a Fashion Category Bootstrap Responsive website Template | Home :: w3layouts</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Footwear Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	<title>Footwear a Fashion Category Bootstrap Responsive website Template | Home :: w3layouts</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Footwear Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
 	SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- Custom Theme files -->
-<link href="<?php echo base_url('assets_home/') ?>css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
-<link href="<?php echo base_url('assets_home/') ?>css/style.css" type="text/css" rel="stylesheet" media="all">   
-<link href="<?php echo base_url('assets_home/') ?>css/font-awesome.css" rel="stylesheet"> <!-- font-awesome icons -->
-<link rel="stylesheet" href="<?php echo base_url('assets_home/') ?>css/owl.carousel.css" type="text/css" media="all"/> <!-- Owl-Carousel-CSS --> 
-<link href='<?php echo base_url('assets_home/') ?>css/simplelightbox.css' rel='stylesheet' type='text/css'>  
-<!-- //Custom Theme files --> 
-<!-- js -->
-<script src="<?php echo base_url('assets_home/') ?>js/jquery-2.2.3.min.js"></script>  
-<!-- //js -->
-<!-- web-fonts -->  
-<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700" rel="stylesheet">
-<link href="//fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
-<!-- //web-fonts -->
+	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<!-- Custom Theme files -->
+	<link href="<?php echo base_url('assets_home/') ?>css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+	<link href="<?php echo base_url('assets_home/') ?>css/style.css" type="text/css" rel="stylesheet" media="all">   
+	<link href="<?php echo base_url('assets_home/') ?>css/font-awesome.css" rel="stylesheet"> <!-- font-awesome icons -->
+	<link rel="stylesheet" href="<?php echo base_url('assets_home/') ?>css/owl.carousel.css" type="text/css" media="all"/> <!-- Owl-Carousel-CSS --> 
+	<link href='<?php echo base_url('assets_home/') ?>css/simplelightbox.css' rel='stylesheet' type='text/css'>  
+	<!-- //Custom Theme files --> 
+	<!-- js -->
+	<script src="<?php echo base_url('assets_home/') ?>js/jquery-2.2.3.min.js"></script>  
+	<!-- //js -->
+	<!-- web-fonts -->  
+	<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+	<!-- //web-fonts -->
 </head>
 <body> 
 	<!-- banner -->
@@ -82,6 +82,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				<div class="banner-w3lstext"> 
 					<h1><a href="index.html">Footwear<span>Welcome To Trendy</span></a></h1>
 					<p>Nulla ultricies nunc et lorem semper quis accumsan dui aliquam aucibus sagittis placerat quis accumsan</p>
+
 				</div> 
 			</div>
 		</div>
@@ -95,26 +96,30 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			<h3 class="agileinfo_title"><span>Product</span> Found (Search "<?php echo $this->input->post('search') ?>")</h3> 
 			<div class="team-row-agileinfo">
 				<?php foreach ($sepatu as $key => $value): ?>
-				<div class="col-md-3 col-sm-6 col-xs-6 team-grids">
-					
+					<div class="col-md-3 col-sm-6 col-xs-6 team-grids">
+
 						
-					
-					<div class="thumbnail team-agileits bg-dark">
-						<img src="<?php echo base_url('uploads/'.$value->image) ?>" class="img-responsive" style="max-height: 200px;min-height: 200px;" alt=""/>
-						<div class="w3agile-caption">
-							<h4><?php echo $value->nama ?></h4>
-							<p><?php echo "Rp.".$value->harga ?></p> 
-							<div class="social-w3lsicon"> 
-								<a href="<?php echo base_url('index.php/Transaksi/add_cart/'.$value->id) ?>"><i class="fa fa-cart-plus"></i></a>  
-							</div>	
-						</div> 
+
+						<div class="thumbnail team-agileits bg-dark">
+							<img src="<?php echo base_url('uploads/'.$value->image) ?>" class="img-responsive" style="max-height: 200px;min-height: 200px;" alt=""/>
+							<div class="w3agile-caption">
+								<h4><?php echo $value->nama ?></h4>
+								<p><?php echo "Rp.".$value->harga ?></p> 
+								<p><?php echo "Ukuran ".$value->ukuran ?></p> 
+								<div class="social-w3lsicon"> 
+									<a href="<?php echo base_url('index.php/Transaksi/add_cart/'.$value->id) ?>"><i class="fa fa-cart-plus"></i></a>  
+									<button type="button" class="btn btn-default" data-container="body" data-trigger="hover" data-toggle="popover" data-placement="top" data-content="<?php echo $value->keterangan ?>">
+										Detail
+									</button>
+								</div>	
+							</div> 
+						</div>
+
 					</div>
-					
-				</div>
-				<?php if (($key+1)%4 == 0): ?>
-					<div class="clearfix"></div>
-					<div style="height: 100px;display: block"></div>
-				<?php endif ?>
+					<?php if (($key+1)%4 == 0): ?>
+						<div class="clearfix"></div>
+						<div style="height: 100px;display: block"></div>
+					<?php endif ?>
 				<?php endforeach ?>
 			</div>
 		</div>
@@ -135,43 +140,43 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		<div class="container">  
 			<h3 class="agileinfo_title">Keranjang <span>Belanja</span> </h3> 
 			<?php echo form_open('Transaksi/update_cart'); ?>
-        <table class="table table-striped table-hover table-bordered"> 
-          <thead> 
-            <tr> 
-              <th></th>
-              <th>Nama</th>
-              <th>Jumlah</th>
-              <th>Harga</th>
-              <th>Sub Total</th>
-              <th>#</th>
-            </tr>
-          </thead>
-          <tbody> 
-            <?php $i = 1; ?>
-            <?php foreach ($this->cart->contents() as $key => $value): ?>
-              <?php echo form_hidden($i.'[rowid]', $value['rowid']); ?>
-              <tr>  
-                <td>
-                  <img class="mr-3" src="<?php echo base_url('uploads/'.$value['image']) ?>" alt="Generic placeholder image" style="width: 30%;max-height: 200px;">
-                </td>
-                <td><?php echo $value['name'] ?></td>
-                <td><?php echo form_input(array('name' => $i.'[qty]', 'value' => $value['qty'], 'maxlength' => '3', 'size' => '5','class'=>'form-control')); ?></td>
-                <td><?php  echo $value['price'] ?></td>
-                <td><?php   echo $value['subtotal'] ?></td>
-                <td><a href="<?php echo site_url('Transaksi/delete_cart/'.$value['rowid']) ?>" class="btn btn-sm btn-danger">Delete</a></td>
-              </tr>
-              <?php $i++; ?>
-            <?php endforeach ?>
-          </tbody>
-        </table>
-        <?php if (count($this->cart->contents())): ?>
-          <?php echo form_submit('', 'Update your Cart',array('class' => 'btn btn-success')); ?>
-          <a href="<?php echo base_url("Transaksi/reset_cart") ?>" class="btn btn-danger">Remove All</a>
-          <a href="<?php echo base_url("Transaksi/checkout") ?>" class="btn btn-primary float-right">Bayar (Total <?php echo $this->cart->total(); ?>)</a>
-        <?php else: ?>
-          <a href="<?php echo base_url("") ?>" class="btn btn-info">Cart Kosong</a>
-        <?php endif ?>
-        <?php echo form_close(); ?>
+			<table class="table table-striped table-hover table-bordered"> 
+				<thead> 
+					<tr> 
+						<th></th>
+						<th>Nama</th>
+						<th>Jumlah</th>
+						<th>Harga</th>
+						<th>Sub Total</th>
+						<th>#</th>
+					</tr>
+				</thead>
+				<tbody> 
+					<?php $i = 1; ?>
+					<?php foreach ($this->cart->contents() as $key => $value): ?>
+						<?php echo form_hidden($i.'[rowid]', $value['rowid']); ?>
+						<tr>  
+							<td>
+								<img class="mr-3" src="<?php echo base_url('uploads/'.$value['image']) ?>" alt="Generic placeholder image" style="width: 30%;max-height: 200px;">
+							</td>
+							<td><?php echo $value['name'] ?></td>
+							<td><?php echo form_input(array('name' => $i.'[qty]', 'value' => $value['qty'], 'maxlength' => '3', 'size' => '5','class'=>'form-control')); ?></td>
+							<td><?php  echo $value['price'] ?></td>
+							<td><?php   echo $value['subtotal'] ?></td>
+							<td><a href="<?php echo site_url('Transaksi/delete_cart/'.$value['rowid']) ?>" class="btn btn-sm btn-danger">Delete</a></td>
+						</tr>
+						<?php $i++; ?>
+					<?php endforeach ?>
+				</tbody>
+			</table>
+			<?php if (count($this->cart->contents())): ?>
+				<?php echo form_submit('', 'Update your Cart',array('class' => 'btn btn-success')); ?>
+				<a href="<?php echo base_url("Transaksi/reset_cart") ?>" class="btn btn-danger">Remove All</a>
+				<a href="<?php echo base_url("Transaksi/checkout") ?>" class="btn btn-primary float-right">Bayar (Total <?php echo $this->cart->total(); ?>)</a>
+			<?php else: ?>
+				<a href="<?php echo base_url("") ?>" class="btn btn-info">Cart Kosong</a>
+			<?php endif ?>
+			<?php echo form_close(); ?>
 		</div>
 	</div>
 	<!-- //blog --> 
@@ -232,13 +237,13 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	<script type="text/javascript" src="<?php echo base_url('assets_home/') ?>js/move-top.js"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets_home/') ?>js/easing.js"></script>	
 	<script type="text/javascript">
-			jQuery(document).ready(function($) {
-				$(".scroll").click(function(event){		
-					event.preventDefault();
-			
-			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-				});
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
 			});
+		});
 	</script>
 	<!-- //end-smooth-scrolling -->	
 	<!-- smooth-scrolling-of-move-up -->
@@ -259,8 +264,12 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	</script>
 	<!-- //smooth-scrolling-of-move-up -->    
 	<!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php echo base_url('assets_home/') ?>js/bootstrap.js"></script>
-</body>
-</html>
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="<?php echo base_url('assets_home/') ?>js/bootstrap.js"></script>
+	<script type="text/javascript">
+		$(function () {
+			$('[data-toggle="popover"]').popover()
+		})</script>
+	</body>
+	</html>
